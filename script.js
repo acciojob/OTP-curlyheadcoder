d0753a09ee2bd8653b0c057d15e0f719eff69e35
curlyheadcoder <mayanksonwani078@gmail.com>
2025-06-23 20:12:32 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,15 @@
+const codes = document.querySelectorAll(".code");
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+  code.addEventListener("input", (event) => {
+    if (event.target.value.length === 1 && idx < codes.length - 1) {
+      codes[idx + 1].focus();
+    }
+  });
+  code.addEventListener("keydown", (event) => {
+    if (event.key === "Backspace" && !event.target.value && idx > 0) {
+      codes[idx - 1].focus();
+    }
+  });
+});
